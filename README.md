ee777aad6f67d2665f8f6b182db0426e25c74851
ashifanassar <ashifanassar@gmail.com>
2024-05-04 05:02:45 +0530

    module

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tf-module-alb
+
+This repo contains backend module to create the load balancers
---
+++ sg.tf
@@ -0,0 +1,52 @@
+# security groups that allows only ws netwokr and internal network
+resource "aws_security_group" "allow_public" {
+  name                    = "roboshop-${var.ENV}-public-alb-sg"
+  description             = "roboshop-${var.ENV}-public-alb--sg"
+  vpc_id                  = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-alb-sg"
+  }
+}
+
+
+# security groups that allows only ws netwokr and internal network
+resource "aws_security_group" "allow_private" {
+  name                    = "roboshop-${var.ENV}-private-alb-sg"
+  description             = "roboshop-${var.ENV}-private-alb-sg"
+  vpc_id                  = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR, data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-private-alb-sg"
+  }
+}
