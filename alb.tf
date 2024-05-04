fad38308b96f5bd3365aceb737eec3b3e6d83d6b
ashifanassar <ashifanassar@gmail.com>
2024-05-04 23:07:07 +0530

    modules

--- alb.tf
+++ alb.tf
@@ -1,15 +1,20 @@
 # Code for creation of alb
 
 resource "aws_lb" "alb" {
-  name               = "test-lb-tf"
-  internal           = false
+  name               = var.ALB_NAME
+  internal           = var.INTERNAL
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  security_groups    = var.INTERNAL ? aws_security_group.allow_private.*.id  : aws_security_group.allow_public.*.id
+  subnets            = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
+
 
   enable_deletion_protection = false
 
   tags = {
-    Environment = "production"
+    Environment = var.ALB_NAME
   }
 }
+
+
+# Public-Module  --->  Public ALB  + Public SG  + Public Subnets 
+# Provate-Module --->  Private ALB + Private SG + Private Subnets 
